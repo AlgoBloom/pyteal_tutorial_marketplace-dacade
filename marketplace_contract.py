bb56818c0b98fc6613cc459c495ed4597e2d8bea
Joshua Bloom <bloomblockchain@gmail.com>
2022-09-25 19:05:25 -0400

    Filled out the product variables subclass with name, image, description, price and sold.

--- marketplace_contract.py
+++ marketplace_contract.py
@@ -1,1 +1,19 @@
+from pydoc import describe
 from pyteal import *
+
+# we create a product class in order to make this code reuseable
+class Product:
+    # subclass variables defines the keys for our global state variable
+    class Variables:
+        # name of the product
+        name = Bytes("NAME")
+        # image for the product
+        image = Bytes("IMAGE")
+        # description of a product
+        description = Bytes("DESCRIPTION")
+        # price of a product
+        price = Bytes("PRICE")
+        # has this product been sold?
+        sold = Bytes("SOLD")
+    # subclass app methods deines methods available for the product
+    class AppMethods:
