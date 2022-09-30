d7e3dda4a0a1af6a31b1ff294936387cd3fc2d11
Joshua Bloom <bloomblockchain@gmail.com>
2022-09-30 19:40:29 -0400

    Added subroutine which first checks if the name of the program is main, this should always be true. Next we added the first thing that happens in this subroutine. The approval program is set to the approval program we wrote in the product class in our marketplace tutorial pyteal smart contract.

--- compile_contract.py
+++ compile_contract.py
@@ -3,3 +3,10 @@ from pyteal import *
 
 # importing the Product class from the marketplace contract
 from marketplace_contract import Product
+
+# this should be true
+if __name__ == "__main__":
+    # defining our approval program
+    approval_program = Product().approval_program()
+    # defininf our clear program
+    clear_program = Product().clear_program()
